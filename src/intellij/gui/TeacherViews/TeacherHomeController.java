39dcff7248e5b20e8e33a735a5fc79ef7c1cd207
Ninkuk <ninadk03@gmail.com>
2019-01-10 09:36:48 -0700

    removed redundant code and reformatted code

--- src/intellij/gui/TeacherViews/TeacherHomeController.java
+++ src/intellij/gui/TeacherViews/TeacherHomeController.java
@@ -1,21 +1,19 @@
 package intellij.gui.TeacherViews;
 
-import intellij.Book;
 import intellij.Student;
 import intellij.database.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class TeacherHomeController implements Initializable {
